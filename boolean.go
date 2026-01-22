d6c99119bf27bc9824a2906c9063935b0524270e
machage9603 <mikemachage@gmail.com>
2026-01-22 13:01:31 +0300

    George Boole

---
+++ boolean.go
@@ -0,0 +1,16 @@
+package main 
+
+import "fmt"
+
+func main(){
+	boolean()
+}
+
+func boolean(){
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
