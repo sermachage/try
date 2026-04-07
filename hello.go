eb82933274dae4a28bebebac8dd34dbf3254801c
machage9603 <mikemachage@gmail.com>
2026-04-07 22:10:07 +0300

    finally installed vs

--- hello.go
+++ hello.go
@@ -5,3 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// To run this code, save it in a file named hello.go and execute the following command in your terminal:
+// go run hello.go
