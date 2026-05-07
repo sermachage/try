c54f12f321b1f668c7fe96459f48fa65c2cb5f3e
sermachage <mikemachage@gmail.com>
2026-05-07 10:03:25 +0300

    clean:remove redudant files

--- collatz.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func collatz(n int) int {
-	if n%2 == 0 {
-		return n / 2
-	} else {
-		return 3*n + 1
-	}
-}
--- hello.go
+++ hello.go
@@ -3,8 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello, mfs," + " Iam back!")
 }
-
-// To run this code, save it in a file named hello.go and execute the following command in your terminal:
-// go run hello.go
